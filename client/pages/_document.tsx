e6f617da69dcd25e1830c05800473727f0cc0c61
marie <max.aschmann@gmail.com>
2024-05-26 17:47:54 +0200

    Fix syntax error

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -93,7 +93,7 @@ class AppDocument extends Document<Props> {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-E9SEGJDFHL"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
 
           gtag('config', 'G-E9SEGJDFHL');
